34470d8c68b32a69f37f3ee47cbd99be1aaab0c1
Danilo Camargo <dno_kaneda@hotmail.com>
2020-01-18 17:46:49 -0300

    Feat(App): DevRadar

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { StatusBar } from 'react-native'
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Fragment >
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7"/>
+      <Routes />
+    </Fragment>
+  );
+}
